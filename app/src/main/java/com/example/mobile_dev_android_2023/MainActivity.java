ffa721a1f6621214b8f6df32adfd81bbf18bf84a
samratTBC <samratjung12@gmail.com>
2023-02-26 22:37:11 +0545

    Initial Android Studio and Project Setup

---
+++ app/src/main/java/com/example/mobile_dev_android_2023/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobile_dev_android_2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
